c6bd17337e057ad13b6b5e5b147edbc9d3fc98ec
Mert Acel <mert.acel@ardictech.com>
2019-04-17 14:30:42 +0300

    CassandraDB Sablonu Eklendi

--- 03_Veri_Tabani/06_CassandraDB/main.go
+++ 03_Veri_Tabani/06_CassandraDB/main.go
@@ -1,4 +1,4 @@
-/* Before you execute the program, Launch `cqlsh` and execute:
+/*Before you execute the program, Launch `cqlsh` and execute:
 create keyspace example with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
 create table example.tweet(timeline text, id UUID, text text, PRIMARY KEY(id));
 create index on example.tweet(timeline);

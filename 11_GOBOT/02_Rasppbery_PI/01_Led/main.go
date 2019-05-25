8501229468f3338263a5e37850b7653a379e31b2
Mert Acel <mertacel@gmail.com>
2019-05-26 02:33:06 +0300

    GOBOT duzenlendi

--- 11_GOBOT/02_Rasppbery_PI/01_Led/main.go
+++ 11_GOBOT/02_Rasppbery_PI/01_Led/main.go
@@ -3,28 +3,39 @@ package main
 import (
 	"time"
 
-	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
 func main() {
-	r := raspi.NewAdaptor()
-	led := gpio.NewLedDriver(r, "3")
 
-	work := func() {
-		gobot.Every(1*time.Second, func() {
-			led.Toggle()
-		})
+	r := raspi.NewAdaptor()
+	gpioPin := gpio.NewDirectPinDriver(r, "3")
+
+	for {
+		gpioPin.DigitalWrite(1)
+		time.Sleep(3 * time.Second)
+		gpioPin.DigitalWrite(0)
+		time.Sleep(3 * time.Second)
 	}
 
-	robot := gobot.NewRobot("blinkBot",
-		[]gobot.Connection{r},
-		[]gobot.Device{led},
-		work,
-	)
+	/*
+		r := raspi.NewAdaptor()
+		led := gpio.NewLedDriver(r, "3")
 
-	robot.Start()
+		work := func() {
+			gobot.Every(1*time.Second, func() {
+				led.Toggle()
+			})
+		}
+
+		robot := gobot.NewRobot("blinkBot",
+			[]gobot.Connection{r},
+			[]gobot.Device{led},
+			work,
+		)
+
+		robot.Start()*/
 }
 
 // GOARM=7 GOARCH=arm GOOS=linux go build main.go
